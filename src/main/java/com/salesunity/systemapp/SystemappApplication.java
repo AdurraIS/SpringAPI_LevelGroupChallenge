774c339fbe1cfdce2fea8bde6d93a493bf82305b
Giovanni Ultramari <giovanniultramari@gmail.com>
2024-03-28 00:32:16 -0300

    Initial Commit

---
+++ src/main/java/com/salesunity/systemapp/SystemappApplication.java
@@ -0,0 +1,13 @@
+package com.salesunity.systemapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemappApplication.class, args);
+	}
+
+}
