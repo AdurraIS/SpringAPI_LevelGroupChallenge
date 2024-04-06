2bf7b9acc8da72c9cf6bc47e88289b81896613a5
Giovanni Ultramari <giovanniultramari@gmail.com>
2024-04-06 14:45:56 -0300

    FEAT: Creating Authentication Controller (Login/Register)

---
+++ src/main/java/com/salesunity/systemapp/controller/AuthenticationController.java
@@ -0,0 +1,58 @@
+package com.salesunity.systemapp.controller;
+
+import com.salesunity.systemapp.dto.AuthenticationDTO;
+import com.salesunity.systemapp.dto.RegisterDTO;
+import com.salesunity.systemapp.model.Usuario;
+import com.salesunity.systemapp.model.roles.UsuarioRoles;
+import com.salesunity.systemapp.repository.EmpresaRepository;
+import com.salesunity.systemapp.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("auth")
+public class AuthenticationController {
+
+    private final AuthenticationManager authenticationManager;
+    private final UsuarioRepository usuarioRepository;
+    private final EmpresaRepository empresaRepository;
+
+    @Autowired
+    public AuthenticationController(AuthenticationManager authenticationManager, UsuarioRepository usuarioRepository, EmpresaRepository empresaRepository) {
+        this.authenticationManager = authenticationManager;
+        this.usuarioRepository = usuarioRepository;
+        this.empresaRepository = empresaRepository;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody @Validated AuthenticationDTO data){
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.getEmail(), data.getSenha());
+        var auth = this.authenticationManager.authenticate(usernamePassword);
+        return ResponseEntity.ok().build();
+    }
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody @Validated RegisterDTO registerDTO){
+        if(this.usuarioRepository.findByEmail(registerDTO.getEmail()) != null){
+            return ResponseEntity.badRequest().build();
+        }
+        String encryptedPassword = new BCryptPasswordEncoder().encode(registerDTO.getSenha());
+        Usuario usuario = new Usuario();
+        usuario.setName(registerDTO.getName());
+        usuario.setEmail(registerDTO.getEmail());
+        usuario.setRole(UsuarioRoles.valueOf(registerDTO.getRole()));
+        usuario.setSenha(encryptedPassword);
+        usuario.setEmpresa(empresaRepository.findById(registerDTO.getEmpresa_id()).orElseThrow());
+        this.usuarioRepository.save(usuario);
+        return ResponseEntity.ok().build();
+    }
+
+}
---
+++ src/main/java/com/salesunity/systemapp/dto/AuthenticationDTO.java
@@ -0,0 +1,11 @@
+package com.salesunity.systemapp.dto;
+
+import lombok.Getter;
+
+@Getter
+public class AuthenticationDTO {
+
+    private String email;
+    private String senha;
+
+}
