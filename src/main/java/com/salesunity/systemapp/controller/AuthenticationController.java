534bd154c247a3cb3e88316b9cbdc159493f6d6e
Giovanni Ultramari <giovanniultramari@gmail.com>
2024-04-06 23:05:55 -0300

    FEAT: Adding JWT Token

--- src/main/java/com/salesunity/systemapp/controller/AuthenticationController.java
+++ src/main/java/com/salesunity/systemapp/controller/AuthenticationController.java
@@ -1,7 +1,9 @@
 package com.salesunity.systemapp.controller;
 
 import com.salesunity.systemapp.dto.AuthenticationDTO;
+import com.salesunity.systemapp.dto.LoginResponseDTO;
 import com.salesunity.systemapp.dto.RegisterDTO;
+import com.salesunity.systemapp.infra.security.TokenService;
 import com.salesunity.systemapp.model.Usuario;
 import com.salesunity.systemapp.model.roles.UsuarioRoles;
 import com.salesunity.systemapp.repository.EmpresaRepository;
@@ -25,19 +27,23 @@ public class AuthenticationController {
     private final AuthenticationManager authenticationManager;
     private final UsuarioRepository usuarioRepository;
     private final EmpresaRepository empresaRepository;
+    private final TokenService tokenService;
 
     @Autowired
-    public AuthenticationController(AuthenticationManager authenticationManager, UsuarioRepository usuarioRepository, EmpresaRepository empresaRepository) {
+    public AuthenticationController(AuthenticationManager authenticationManager, UsuarioRepository usuarioRepository, EmpresaRepository empresaRepository, TokenService tokenService) {
         this.authenticationManager = authenticationManager;
         this.usuarioRepository = usuarioRepository;
         this.empresaRepository = empresaRepository;
+        this.tokenService = tokenService;
     }
 
     @PostMapping("/login")
-    public ResponseEntity login(@RequestBody @Validated AuthenticationDTO data){
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody @Validated AuthenticationDTO data){
         var usernamePassword = new UsernamePasswordAuthenticationToken(data.getEmail(), data.getSenha());
         var auth = this.authenticationManager.authenticate(usernamePassword);
-        return ResponseEntity.ok().build();
+        var token = tokenService.generateToken((Usuario) auth.getPrincipal());
+
+        return ResponseEntity.ok(new LoginResponseDTO(token));
     }
     @PostMapping("/register")
     public ResponseEntity register(@RequestBody @Validated RegisterDTO registerDTO){
--- src/main/java/com/salesunity/systemapp/infra/security/SecurityConfigurations.java
+++ src/main/java/com/salesunity/systemapp/infra/security/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package com.salesunity.systemapp.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,11 +12,13 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
-
+    @Autowired
+    SecurityFilter securityFilter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         return httpSecurity
@@ -27,6 +30,7 @@ public class SecurityConfigurations {
                         .requestMatchers(HttpMethod.POST, "/produtos").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
     @Bean
